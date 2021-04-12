dd163585e2ff2678cd597d3d1009fc65e44dcd43
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-04-12 19:12:33 +0300

    signin signout

--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-sign-up/singn-in-sign-up.component'
+import {auth} from './firebase/firebase.utils'
 
 
-function App() {
-  return (
-    <div >
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
-      </Switch>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      currentUser: null
+    }
+  }
+
+  unsubscribefromAuth =null
+
+  componentDidMount(){
+    this.unsubscribefromAuth=auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribefromAuth();
+  }
+
+  render(){
+
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage}/>
+        </Switch>
+        
+      </div>
+    )
+  };
 }
 
 export default App;
