4674f70e34da9ab550becac9bc5a5b5149154026
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-04-15 12:31:16 +0300

    cart add items

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,15 @@
-export const additemToCart = (cartItems, cartItemToAdd)=>{
-    const existingCartItem = cartItemToAdd.find(
+export const addItemToCart = (cartItems, cartItemToAdd)=>{
+    const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
     );
 
     if(existingCartItem){
         return cartItems.map(cartItem=>
             cartItem.id === cartItemToAdd.id
-            ? {...cartItem, quantity: cartItem.quantity +1}
+            ? {...cartItem, quantity: cartItem.quantity + 1}
             : cartItem
             )
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity:1 }]
+    return [...cartItems, {...cartItemToAdd, quantity: 1 }]
 }
